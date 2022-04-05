6c23d30ada7382a53b3ceec45979bf2d905d1afe
Victor Piolin <victor.piolin@gmail.com>
2022-04-04 23:40:40 -0400

    Add some indentation for switch / case

--- main.go
+++ main.go
@@ -36,49 +36,48 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
 
-    // Is it a key press?
-    case tea.KeyMsg:
+        // Is it a key press?
+        case tea.KeyMsg:
+            // Cool, what was the actual key pressed?
+            switch msg.String() {
 
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
+                // These keys should exit the program.
+                case "ctrl+c", "q":
+                    return m, tea.Quit
 
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            return m, tea.Quit
+                // The "up" and "k" keys move the cursor up
+                case "up", "k":
+                    if m.cursor > 0 {
+                        m.cursor--
+                    }
 
-        // The "up" and "k" keys move the cursor up
-        case "up", "k":
-            if m.cursor > 0 {
-                m.cursor--
+                // The "down" and "j" keys move the cursor down
+                case "down", "j":
+                    if m.cursor < len(m.previousCopy)-1 {
+                        m.cursor++
+                    }
+
+                // The "enter" key and the spacebar (a literal space) toggle
+                // the selected state for the item that the cursor is pointing at.
+                case "enter", " ":
+                    _, ok := m.selected[m.cursor]
+                    if ok {
+                        delete(m.selected, m.cursor)
+                    } else {
+                        m.selected[m.cursor] = struct{}{}
+                    }
             }
 
-        // The "down" and "j" keys move the cursor down
-        case "down", "j":
-            if m.cursor < len(m.previousCopy)-1 {
-                m.cursor++
-            }
-
-        // The "enter" key and the spacebar (a literal space) toggle
-        // the selected state for the item that the cursor is pointing at.
-        case "enter", " ":
-            _, ok := m.selected[m.cursor]
-            if ok {
-                delete(m.selected, m.cursor)
+        case string:
+            if (len(m.previousCopy) == 0) {
+                m.previousCopy = append(m.previousCopy, msg)
             } else {
-                m.selected[m.cursor] = struct{}{}
+                latestCopy := m.previousCopy[len(m.previousCopy) -1]
+
+                if (latestCopy != msg) {
+                    m.previousCopy = append(m.previousCopy, msg)
+                }
             }
-        }
-
-	case string:
-		if (len(m.previousCopy) == 0) {
-			m.previousCopy = append(m.previousCopy, msg)
-		} else {
-			latestCopy := m.previousCopy[len(m.previousCopy) -1]
-
-			if (latestCopy != msg) {
-				m.previousCopy = append(m.previousCopy, msg)
-			}
-		}
 	}
 
     // Return the updated model to the Bubble Tea runtime for processing.
