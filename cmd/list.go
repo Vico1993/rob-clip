b66fd5b92f7fd7a51f78321758cbb62b18966f31
Victor Piolin <victor.piolin@gmail.com>
2022-05-12 00:12:49 -0400

    Small update

--- cmd/list.go
+++ cmd/list.go
@@ -113,24 +113,19 @@ func (c clipboardHistory) Init() tea.Cmd {
     return nil
 }
 
-func initialModel(l []string) clipboardHistory {
-	return clipboardHistory{
-		list:  l,
-
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
-	}
-}
-
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "List all copy from your past",
 	Run: func(cmd *cobra.Command, args []string) {
 		list := viper.GetStringSlice("daemon_word")
 
-		p := tea.NewProgram(initialModel(list))
+		// Initialisation of the tea program
+		p := tea.NewProgram(
+			clipboardHistory{
+				list:  list,
+				selected: make(map[int]struct{}),
+			},
+		)
 
 		viper.OnConfigChange(func(e fsnotify.Event) {
 			newlist := viper.GetStringSlice("daemon_word")
